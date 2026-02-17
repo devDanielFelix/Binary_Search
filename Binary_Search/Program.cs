15164e43ea739e461fc1ee598f75424a77640186
devDanielFelix <danielfelix72@hotmail.com>
2026-02-17 14:40:09 -0300

    Add project files.

---
+++ Binary_Search/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static int BinarySearch(int[] array, int valor)
+    {
+        int esquerdo = 0;
+        int direito = array.Length - 1;
+
+        while (esquerdo <= direito)
+        {
+            int meio = esquerdo + (direito - esquerdo) / 2;
+
+            if (array[meio] == valor)
+            {
+                return meio; // Valor encontrado, retorna o índice
+            }
+            else if (array[meio] < valor)
+            {
+                esquerdo = meio + 1; // Continua a busca na metade direita
+            }
+            else
+            {
+                direito = meio - 1; // Continua a busca na metade esquerda
+            }
+        }
+        return -1; // Valor não encontrado
+    }
+
+    static void Main(string[] args)
+    {
+        // ponto de entrada válido. Chame o BinarySearch a partir daqui ou de outro código conforme necessário.
+    }
+}
